292855e6092c23b5aa75036aa72d33299026e1ae
“[Levy <103933738+levyCro@users.noreply.github.com>
2022-10-14 10:25:32 +0200

    adding twig file

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,7 +11,18 @@ class VinylController extends AbstractController
     #[Route('/', name: 'app_vinyl')]
     public function homepage(): Response
     {
-        return New Response('hello world');
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'WaterFalls', 'artist' => 'TLC'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Maria Carey'],
+        ];
+
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks
+        ]);
     }
 
     #[Route('/browse/{slug}', name: 'browse')]
