fef42e0f5c18cfc15be12bdd7232fba4168a7dc1
“[Levy <103933738+levyCro@users.noreply.github.com>
2022-10-20 09:29:46 +0200

    adding playlist

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class VinylController extends AbstractController
 {
     #[Route('/', name: 'app_vinyl')]
@@ -18,22 +19,22 @@ class VinylController extends AbstractController
             ['song' => 'On Bended knee', 'artist' => 'Boyz II Men'],
             ['song' => 'Fantasy', 'artist' => 'Maria Carey'],
         ];
+        
 
         return $this->render('vinyl/homepage.html.twig', [
             'title' => 'PB & Jams',
             'tracks' => $tracks
         ]);
+        
     }
 
-    #[Route('/browse/{slug}', name: 'browse')]
+    #[Route('/browse/{slug}', name: 'app_browse')]
     public function browse(string $slug = null): Response
     {
-        if($slug){
-            $title = 'Genre: '.ucwords(str_replace('-', ' ', $slug));
-        } else {
-            $title = 'All Genres';
-        }
+        $genre = $slug ? ucwords(str_replace('-', ' ', $slug)) : null;
         
-        return new Response($title);
+        return $this->render('vinyl/browse.html.twig', [
+            'genre' => $genre,
+        ]);
     }
 }
