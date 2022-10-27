6e9583f3a1517309d9b4523168b134fd825a4e0b
“[Levy <103933738+levyCro@users.noreply.github.com>
2022-10-27 15:14:07 +0200

    adding vinyl info on the browse page and installing knpDateFormatter

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
+
 class VinylController extends AbstractController
 {
     #[Route('/', name: 'app_vinyl')]
@@ -32,9 +33,37 @@ class VinylController extends AbstractController
     public function browse(string $slug = null): Response
     {
         $genre = $slug ? ucwords(str_replace('-', ' ', $slug)) : null;
+        $mixes = $this->getMixes();
         
+
         return $this->render('vinyl/browse.html.twig', [
             'genre' => $genre,
+            'mixes' => $mixes,
         ]);
+    }
+
+    private function getMixes(): array
+    {
+        // fake mixes
+        return [
+            [
+                'title' => 'PB & Jams',
+                'trackCount' => 14,
+                'genre' => 'Rock',
+                'createdAt' => new \DateTime('2021-10-02'),
+            ],
+            [
+                'title' => 'Put a Hex o your Ex',
+                'trackCount' => 8,
+                'genre' => 'Heavy Metal',
+                'createdAt' => new \DateTime('2022-04-22'),
+            ],
+            [
+                'title' => 'Spice Girls - Summer Tunes',
+                'trackCount' => 10,
+                'genre' => 'Pop',
+                'createdAt' => new \DateTime('2020-06-02'),
+            ],
+        ];
     }
 }
